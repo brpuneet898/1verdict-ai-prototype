daccd3314730606506d7c29cd8274ff53ae89c9c
Puneet <brpuneet898@gmail.com>
2025-07-25 14:43:27 +0530

    added register page

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask
+from routes import main
 
 app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return render_template("index.html")
+app.register_blueprint(main)
 
 if __name__ == "__main__":
     app.run(debug=True)
